6323b43ea70e942a1d4166082c1a3ae8e30c1ca1
marta.rod <rod21062@byui.edu>
2025-01-16 22:41:44 -0500

    Core 2 & 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,28 +1,39 @@
 using System;
 using System.Net;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is the magic number? ");
+        /*Console.Write("What is the magic number? ");
         string magicNumber = Console.ReadLine();
-        int number = int.Parse(magicNumber);
-        Console.Write("What is your guess? ");
-        string guessNumber = Console.ReadLine();
-        int guess = int.Parse(guessNumber);
+        int number = int.Parse(magicNumber);*/
 
-        if (guess > number)
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        int guess;
+
+        do
         {
-            Console.WriteLine("Lower.");
+            Console.Write("What is your guess? ");
+            string guessNumber = Console.ReadLine();
+            guess = int.Parse(guessNumber);
+
+            if (guess > number)
+            {
+                Console.WriteLine("Lower.");
+            }
+            else if (guess < number )
+            {
+                Console.WriteLine("Higher.");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }        
         }
-        else if (guess < number )
-        {
-            Console.WriteLine("Higher.");
-        }
-        else
-        {
-            Console.WriteLine("You guessed it!");
-        }
+        while (guess != number);  
     }
 }
