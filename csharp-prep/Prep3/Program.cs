ee6c3f5d9d8a72ce557f69c9b8cd8f446be97bc4
marta.rod <rod21062@byui.edu>
2025-01-16 21:59:14 -0500

    One Core Challenge

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        string magicNumber = Console.ReadLine();
+        int number = int.Parse(magicNumber);
+        Console.Write("What is your guess? ");
+        string guessNumber = Console.ReadLine();
+        int guess = int.Parse(guessNumber);
+
+        if (guess > number)
+        {
+            Console.WriteLine("Lower.");
+        }
+        else if (guess < number )
+        {
+            Console.WriteLine("Higher.");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
     }
 }
